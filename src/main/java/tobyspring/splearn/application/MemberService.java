2052e804ad0816c81148be92c54a3ccb1051631e
shinkwang <shinkwang@yonsei.ac.kr>
2025-07-18 08:05:30 +0900

    test: DuplicateEmailException in Member

--- src/main/java/tobyspring/splearn/application/MemberService.java
+++ src/main/java/tobyspring/splearn/application/MemberService.java
@@ -5,9 +5,7 @@ import org.springframework.stereotype.Service;
 import tobyspring.splearn.application.provided.MemberRegister;
 import tobyspring.splearn.application.required.EmailSender;
 import tobyspring.splearn.application.required.MemberRepository;
-import tobyspring.splearn.domain.Member;
-import tobyspring.splearn.domain.MemberRegisterRequest;
-import tobyspring.splearn.domain.PasswordEncoder;
+import tobyspring.splearn.domain.*;
 
 @RequiredArgsConstructor
 @Service
@@ -19,7 +17,9 @@ public class MemberService implements MemberRegister {
 
     @Override
     public Member register(MemberRegisterRequest registerRequest) {
-        // check
+        if (memberRepository.findByEmail(new Email(registerRequest.email())).isPresent()) {
+            throw new DuplicateEmailException("이미 사용중인 이메일입니다: " + registerRequest.email());
+        }
 
         Member member = Member.register(registerRequest, passwordEncoder);
 

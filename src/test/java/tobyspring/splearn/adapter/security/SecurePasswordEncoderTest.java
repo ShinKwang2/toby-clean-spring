2d372b306af7bcc8781b9bedfae0377f866cc7bc
kwang <shinkwang@yonsei.ac.kr>
2025-07-22 22:26:01 +0900

    feat: PasswordEncoder by spring-security-core

---
+++ src/test/java/tobyspring/splearn/adapter/security/SecurePasswordEncoderTest.java
@@ -0,0 +1,21 @@
+package tobyspring.splearn.adapter.security;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SecurePasswordEncoderTest {
+
+    @Test
+    void securePasswordEncoder() {
+        SecurePasswordEncoder securePasswordEncoder = new SecurePasswordEncoder();
+
+        String passwordHash = securePasswordEncoder.encode("secret");
+
+        assertThat(securePasswordEncoder.matches("secret", passwordHash)).isTrue();
+        assertThat(securePasswordEncoder.matches("wrong", passwordHash)).isFalse();
+    }
+
+}
